1c85ff4c240c0d2a1c29f34be12c97444528e580
Tyler <tyler.crumpton@gmail.com>
2014-11-30 02:17:15 -0600

    Added modes to set color and enter rainbow mode.

--- snappyImages/lightController.py
+++ snappyImages/lightController.py
@@ -1,10 +1,31 @@
 from synapse.switchboard import *
 
+sendAngle = True
+
 def reportAngle(device, angle):
-    # Right now, we don't do anything with the device name, so ignore it:
-    print chr(angle),
+    global sendAngle
+    # If not in another print mode:
+    if sendAngle:
+        # Since 255 is our special mode value, cap at 254:
+        if angle == 255:
+            angle = 254
+        # Right now, we don't do anything with the device name, so ignore it:
+        print chr(angle),
          
 @setHook(HOOK_STARTUP)   
 def init():
     crossConnect(DS_STDIO, DS_UART1)
     initUart(1, 9600)
+
+def setLightColor(red, green, blue):
+    global sendAngle
+    sendAngle = False
+    print chr(255),chr(1),chr(red),chr(green),chr(blue),
+    sendAngle = True
+    
+def enterRainbowMode():
+    global sendAngle
+    sendAngle = False
+    print chr(255),chr(2),
+    sendAngle = True
+    
