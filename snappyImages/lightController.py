47158377658abd415f016b368fb76601b7ad90d3
Tyler <tyler.crumpton@gmail.com>
2014-12-04 00:02:30 -0600

    Fixing line endings.

--- snappyImages/lightController.py
+++ snappyImages/lightController.py
@@ -16,7 +16,7 @@ def reportAngle(device, angle):
             angle = 254
         # Right now, we don't do anything with the device name, so just send the angle:
         print chr(angle),
-         
+
 @setHook(HOOK_STARTUP)   
 def init():
     crossConnect(DS_STDIO, DS_UART1)
@@ -27,10 +27,9 @@ def setLightColor(red, green, blue):
     sendAngle = False
     print LISTEN,SET_COLOR,chr(red),chr(green),chr(blue),
     sendAngle = True
-    
+
 def enterRainbowMode():
     global sendAngle
     sendAngle = False
     print LISTEN,RAINBOW_MODE,
     sendAngle = True
-    
