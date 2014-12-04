6fed15cc8def0a2dfd6d58522f3be808e95b66a4
Tyler <tyler.crumpton@gmail.com>
2014-12-03 23:54:43 -0600

    Gave names to the control bytes sent to the Arduino.

--- snappyImages/lightController.py
+++ snappyImages/lightController.py
@@ -2,6 +2,11 @@ from synapse.switchboard import *
 
 sendAngle = True
 
+LISTEN       = chr(255)
+SET_COLOR    = chr(1)
+RAINBOW_MODE = chr(2)
+TEAM_MODE    = chr(3)
+
 def reportAngle(device, angle):
     global sendAngle
     # If not in another print mode:
@@ -9,7 +14,7 @@ def reportAngle(device, angle):
         # Since 255 is our special mode value, cap at 254:
         if angle == 255:
             angle = 254
-        # Right now, we don't do anything with the device name, so ignore it:
+        # Right now, we don't do anything with the device name, so just send the angle:
         print chr(angle),
          
 @setHook(HOOK_STARTUP)   
@@ -20,12 +25,12 @@ def init():
 def setLightColor(red, green, blue):
     global sendAngle
     sendAngle = False
-    print chr(255),chr(1),chr(red),chr(green),chr(blue),
+    print LISTEN,SET_COLOR,chr(red),chr(green),chr(blue),
     sendAngle = True
     
 def enterRainbowMode():
     global sendAngle
     sendAngle = False
-    print chr(255),chr(2),
+    print LISTEN,RAINBOW_MODE,
     sendAngle = True
     
