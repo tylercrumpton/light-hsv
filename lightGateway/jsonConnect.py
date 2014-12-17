690dfdf0440a5f2dd3a8c5a1693dbc800054c0bd
Tyler <tyler.crumpton@gmail.com>
2014-12-16 23:40:53 -0600

    Stubbed out a few calls and fleshed out a bit of the rpc(). Added nodejs tester.

--- lightGateway/jsonConnect.py
+++ lightGateway/jsonConnect.py
@@ -1,6 +1,13 @@
+''' JSON Socket wrapper for Synapse SNAP Connect '''
+
 import json
 import socket
 import os
+import snapconnect
+from binascii import unhexlify, hexlify
+
+class AddressError(Exception):
+    pass
 
 sockfile = "./snapconnect.sock"
 
@@ -18,6 +25,31 @@ while True:
 
     data = conn.recv( 1024 )
     if not data:
-        break
+      break
     else:
-        print data
+      try:
+        jdata = json.loads(data)
+        type = jdata['type']
+        if type == 'rpc':
+          dest_address = jdata['address']
+          if len(dest_address) == 6:
+            dest_address = unhexlify(dest_address)
+          else:
+            raise AddressError(dest_address)
+          dest_function = jdata['function']
+          function_args = jdata.get('args', None)
+          print "Calling {0}({1}) on node {2}".format(dest_function, function_args, hexlify(dest_address))
+        elif type == 'mcastrpc':
+          pass
+        elif type == 'hello':
+          pass
+        elif type == 'bye':
+          pass
+        elif type == 'register':
+          pass
+      except KeyError as e:
+        print "Required key {0} not included in message: {1}".format(e, data)
+      except AddressError as e:
+        print "Error in address <{0}>".format(e)
+      except Exception as e:
+        print "Error in decoding message <{0}>: {1}".format(data, e)
