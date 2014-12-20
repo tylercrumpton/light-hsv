f2adff4a7812945f8559098d71e837c7b37d6c67
Tyler <tyler.crumpton@gmail.com>
2014-12-19 22:17:34 -0600

    Refactored a bit and added SNAPConnect polling to loop.

--- lightGateway/jsonConnect.py
+++ lightGateway/jsonConnect.py
@@ -16,7 +16,7 @@ class JSONConnectServer(object):
         self.setupSocket()
     def setupSnapConnect(self, type, port):
         self.sc = snap.Snap()
-        #self.sc.open_serial(type, port)
+        self.sc.open_serial(type, port)
     def setupSocket(self):
         if os.path.exists(self.sockfile):
             os.remove(self.sockfile)
@@ -25,38 +25,41 @@ class JSONConnectServer(object):
     def startServer(self):
         self.s.listen(5)  
         while True:
-          conn, addr = self.s.accept()
-          while True: 
-            data = conn.recv( 1024 )
-            if not data:
-              break
-            else:
-              try:
-                jdata = json.loads(data)
-                type = jdata['type']
-                if type == 'rpc':
-                  dest_address = jdata['address']
-                  if len(dest_address) == 6:
+            conn, addr = self.s.accept()
+            while True: 
+                data = conn.recv( 1024 )
+                if not data:
+                    break
+                else:
+                    self.handleData(data)
+                    self.sc.poll()
+    def handleData(self, data):
+        try:
+            jdata = json.loads(data)
+            type = jdata['type']
+            if type == 'rpc':
+                dest_address = jdata['address']
+                if len(dest_address) == 6:
                     dest_address = unhexlify(dest_address)
-                  else:
+                else:
                     raise AddressError(dest_address)
-                  dest_function = jdata['function']
-                  function_args = jdata.get('args', None)
-                  print "Calling {0}({1}) on node {2}".format(dest_function, function_args, hexlify(dest_address))
-                elif type == 'mcastrpc':
-                  pass
-                elif type == 'hello':
-                  pass
-                elif type == 'bye':
-                  pass
-                elif type == 'register':
-                  pass
-              except KeyError as e:
-                print "Required key {0} not included in message: {1}".format(e, data)
-              except AddressError as e:
-                print "Error in address <{0}>".format(e)
-              except Exception as e:
-                print "Error in decoding message <{0}>: {1}".format(data, e)
+                dest_function = jdata['function']
+                function_args = jdata.get('args', None)
+                print "Calling {0}({1}) on node {2}".format(dest_function, function_args, hexlify(dest_address))
+            elif type == 'mcastrpc':
+                pass
+            elif type == 'hello':
+                pass
+            elif type == 'bye':
+                pass
+            elif type == 'register':
+                pass
+        except KeyError as e:
+            print "Required key {0} not included in message: {1}".format(e, data)
+        except AddressError as e:
+            print "Error in address <{0}>".format(e)
+        except Exception as e:
+            print "Error in decoding message <{0}>: {1}".format(data, e)
                 
 if __name__ == "__main__":
     server = JSONConnectServer()
