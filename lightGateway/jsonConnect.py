da2489f90b29eb8e2be34eabea45914d256ea162
Tyler <tyler.crumpton@gmail.com>
2014-12-23 18:32:06 -0600

    Got the snapconnect and zerorpc loops running simultaneously.

--- lightGateway/jsonConnect.py
+++ lightGateway/jsonConnect.py
@@ -4,8 +4,10 @@ import os
 import zerorpc
 from snapconnect import snap
 from binascii import unhexlify, hexlify
-
+from time import sleep
 import logging
+import gevent
+
 logging.basicConfig()
 
 class SnapRpc(object):
@@ -25,16 +27,21 @@ class SnapRpc(object):
 class JSONConnectServer(object):
     def __init__(self, snap_type=snap.SERIAL_TYPE_RS232, snap_port="/dev/ttys1"):
         self.setupSnapConnect(snap_type, snap_port)
-        self.setupSnapRpc()
+        self.setupZeroRpc()
     def setupSnapConnect(self, type, port):
         self.sc = snap.Snap()
         #self.sc.open_serial(type, port)
-    def setupSnapRpc(self):
+    def setupZeroRpc(self):
         self.zero = zerorpc.Server(SnapRpc(self.sc))
         self.zero.bind("tcp://0.0.0.0:4242")
+    def loopSnapConnect(self):
+        while(1):
+            self.sc.poll()
+            gevent.sleep(0)
     def start(self):
+        gevent.spawn(self.loopSnapConnect)
         self.zero.run()
-                
+
 if __name__ == "__main__":
     server = JSONConnectServer()
     server.start()
--- lightGateway/jsonTest.js
+++ lightGateway/jsonTest.js
@@ -2,18 +2,21 @@
 
 var zerorpc = require("zerorpc");
 
+var server = new zerorpc.Server({
+    randomFunction: function(arg1, arg2) {
+        console.log("Got random function!");
+        reply();
+    }
+    });
 var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
-client.invoke("hello", "World!", function(error, res, more) {
+client.invoke("rpc", "123456", "test_function", "arg1", "arg2", function(error, res, more) {
     console.log(res);
-    client.invoke("rpc", "123456", "test_function", "arg1", "arg2", function(error, res, more) {
+    client.invoke("mcastrpc", "1", "5", "mcast_func", "arg3", "arg4", function(error, res, more) {
         console.log(res);
-        client.invoke("mcastrpc", "1", "5", "mcast_func", "arg3", "arg4", function(error, res, more) {
+        client.invoke("mcastrpc", "1", "5", "mcast_func", function(error, res, more) {
             console.log(res);
-            client.invoke("mcastrpc", "1", "5", "mcast_func", function(error, res, more) {
-                console.log(res);
-            });  
         });  
-    });     
-});
+    });  
+});  
