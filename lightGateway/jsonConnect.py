32115ec4316e8906c22ef23342a2ba2ee0a651bd
Tyler <tyler.crumpton@gmail.com>
2014-12-24 22:37:40 -0600

    Added start of register function work.

--- lightGateway/jsonConnect.py
+++ lightGateway/jsonConnect.py
@@ -11,36 +11,59 @@ import gevent
 logging.basicConfig()
 
 class SnapRpc(object):
-    def __init__(self, sc):
-        self.sc = sc
+    def __init__(self, json_con_server):
+        self.json_con_server = json_con_server
     def rpc(self, address, function, *args):
         if len(address) == 6:
             address = unhexlify(address)
         else:
             return "Error in address {2}".format(address)
-        self.sc.scheduler.schedule(0, self.sc.rpc, address, function, *args)
+        self.json_con_server.sc.scheduler.schedule(0, self.json_con_server.sc.rpc, address, function, *args)
         return "Calling {0}({1}) on node {2}".format(function, args, hexlify(address))
     def mcastrpc(self, group, ttl, function, *args):
-        self.sc.scheduler.schedule(0, self.sc.mcast_rpc, int(group), int(ttl), function, *args)
+        self.json_con_server.sc.scheduler.schedule(0, self.json_con_server.sc.mcast_rpc, int(group), int(ttl), function, *args)
         return "Broadcasting {0}({1}) on mcast group {2} with ttl={3}".format(function, args, group, ttl)
+    def register(self, function):
+        self.json_con_server.sc.scheduler.schedule(0, self.json_con_server.registerFunction, function)
+        return "Registered {0}()".format(function)
+    def list(self):
+        return self.json_con_server.func_dict.keys()
+    def test(self):
+        self.json_con_server.sc.scheduler.schedule(5, self.json_con_server.func_dict["registered_func"], 1,2)
+        return "Trying test:"
 
+
+        
 class JSONConnectServer(object):
     def __init__(self, snap_type=snap.SERIAL_TYPE_RS232, snap_port="/dev/ttys1"):
+        self.func_dict = {}
         self.setupSnapConnect(snap_type, snap_port)
-        self.setupZeroRpc()
+        self.setupZeroRpcServer()
+        self.setupZeroRpcClient()
     def setupSnapConnect(self, type, port):
         self.sc = snap.Snap()
         #self.sc.open_serial(type, port)
-    def setupZeroRpc(self):
-        self.zero = zerorpc.Server(SnapRpc(self.sc))
-        self.zero.bind("tcp://0.0.0.0:4242")
+    def setupZeroRpcServer(self):
+        self.zero_server = zerorpc.Server(SnapRpc(self))
+        self.zero_server.bind("tcp://0.0.0.0:4242")
+    def setupZeroRpcClient(self):
+        self.zero_client = zerorpc.Client()
+        self.zero_client.connect("tcp://127.0.0.1:2424")
     def loopSnapConnect(self):
         while(1):
             self.sc.poll()
             gevent.sleep(0)
     def start(self):
         gevent.spawn(self.loopSnapConnect)
-        self.zero.run()
+        self.zero_server.run()
+    def registerFunction(self, function_name):
+        lambda_func = (lambda *args : self.sendZeroRpc(function_name, *args))
+        self.func_dict[function_name] = lambda_func
+        self.sc.add_rpc_func(function_name, lambda_func)
+    def sendZeroRpc(self, function_name, *args):
+        print "Calling zeroRpc {0}({1})".format(function_name, args)
+        #self.zero_client.function_name(*args)
+        self.zero_client.registered_func(*args)
 
 if __name__ == "__main__":
     server = JSONConnectServer()
--- lightGateway/jsonTest.js
+++ lightGateway/jsonTest.js
@@ -3,11 +3,13 @@
 var zerorpc = require("zerorpc");
 
 var server = new zerorpc.Server({
-    randomFunction: function(arg1, arg2) {
-        console.log("Got random function!");
+    registered_func: function(arg1, arg2, reply) {
+        console.log("Got registered function!");
         reply();
     }
-    });
+});
+server.bind("tcp://0.0.0.0:2424");
+
 var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
@@ -17,6 +19,16 @@ client.invoke("rpc", "123456", "test_function", "arg1", "arg2", function(error, 
         console.log(res);
         client.invoke("mcastrpc", "1", "5", "mcast_func", function(error, res, more) {
             console.log(res);
+            client.invoke("register", "registered_func", function(error, res, more) {
+                console.log(res);
+                client.invoke("list", function(error, res, more) {
+                    console.log(res);
+                    client.invoke("test", function(error, res, more) {
+                        console.log(res);
+                    });  
+                });  
+            });  
+            
         });  
     });  
 });  
