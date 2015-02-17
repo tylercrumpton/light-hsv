a6b8c25ff2c9656a9d38036ba137eefd80517a4d
Nathan W <nathan@frcv.net>
2015-02-16 20:40:28 -0600

    config to enable saving tweets

--- twitter/index.js
+++ twitter/index.js
@@ -1,8 +1,8 @@
-var _       = require('lodash');
-var twitter = require('twitter');
+var _         = require('lodash');
+var twitter   = require('twitter');
 var snaplight = require("./snaplight.js");
-var mongo = require("./mongo.js");
-var config  = require('./config');
+var mongo     = require("./mongo.js");
+var config    = require('./config');
 
 
 var twit = new twitter({
@@ -14,16 +14,16 @@ var twit = new twitter({
 
 twit.stream('statuses/filter', {track: config.twitter.search }, function(stream) {
   stream.on('data', function(data) {
-    // search for hashtag or mention
+    // filter for hashtag or mention
     if ( data.text.match(config.twitter.filter) ){
       parseData(data);
-      mongo.storeData(data);
+      if (config.mongo.enable ) { mongo.storeData(data); } 
     }
   });
 });
 
 
-// Loop over searches looking for a match, then call function if found.
+// Loop through searches looking for a match, then call snap function if found.
 function parseData(data){
   _.forEach(config.searches, function(re) { 
     if ( re.regex.test(data.text) ){
